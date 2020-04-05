165538f7f95b4af8bdf08a7aa17c7b7b197a0b5a
sys72740hro <henry.robben@eur.nl>
2020-04-05 11:46:31 +0200

    diverse verbeteringen.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Entity\ReadMore;
+use App\Repository\ReadMoreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\QuestionRepository;
@@ -20,4 +23,17 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    /**
+     * @Route("defaultreadmore/{id}", name="read_more_default", methods={"GET"})
+     */
+    public function readmore($id, ReadMoreRepository $readMoreRepository): Response
+    {
+        $readMore = $readMoreRepository->findOneBy(['id' => $id]);
+        //$readMore->setContent(htmlentities($readMore->getContent()));
+        return $this->render('default/readmore.html.twig', [
+            'read_more' => $readMore,
+        ]);
+    }
+
 }

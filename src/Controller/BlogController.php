be259c82ee93ff366235496384e4146d4388de02
Riccardo van Dam <ric.v.dam@outlook.com>
2020-05-14 16:11:13 +0200

    blog naars nieuws omgezet. Nieuws afbeelding fixed

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/blog")
+ * @Route("/nieuws")
  */
 class BlogController extends AbstractController
 {
